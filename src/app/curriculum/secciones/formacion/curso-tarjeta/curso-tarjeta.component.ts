53acf499dbc907c267b1b9a4bc0448d760827a03
Facundo Kadir Flores Ferreyra <fkfloresferreyra@outlook.com>
2022-06-17 09:56:04 -0300

    Agregadas tarjetas de educación formal y cursos

--- src/app/curriculum/secciones/formacion/curso-tarjeta/curso-tarjeta.component.ts
+++ src/app/curriculum/secciones/formacion/curso-tarjeta/curso-tarjeta.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input } from '@angular/core';
+import {Curso} from "../../../../common/interfaces/curriculum.interface";
+import {MatDialog} from "@angular/material/dialog";
+import {AgregarCursoComponent} from "../agregar-curso/agregar-curso.component";
 
 @Component({
   selector: 'app-curso-tarjeta',
   templateUrl: './curso-tarjeta.component.html',
   styleUrls: ['./curso-tarjeta.component.css']
 })
-export class CursoTarjetaComponent implements OnInit {
+export class CursoTarjetaComponent  {
 
-  constructor() { }
+  @Input() curso!: Curso;
+  constructor(public dialog: MatDialog) { }
 
-  ngOnInit(): void {
+  editar(id: number | undefined, nombre: string, institucion: string, carga_horaria: string, periodo: Date | undefined, logo?: any) {
+    const dialogRef = this.dialog.open(AgregarCursoComponent, {
+      width: '600px',
+      data: {id: id, nombre: nombre, institucion: institucion, carga_horaria: carga_horaria, periodo: periodo, logo: logo}
+    })
   }
-
 }
--- src/app/curriculum/secciones/formacion/formacion-tarjeta/formacion-tarjeta.component.ts
+++ src/app/curriculum/secciones/formacion/formacion-tarjeta/formacion-tarjeta.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input } from '@angular/core';
+import {Educacion} from "../../../../common/interfaces/curriculum.interface";
+import {MatDialog} from "@angular/material/dialog";
+import {AgregarFormacionComponent} from "../agregar-formacion/agregar-formacion.component";
 
 @Component({
   selector: 'app-formacion-tarjeta',
   templateUrl: './formacion-tarjeta.component.html',
   styleUrls: ['./formacion-tarjeta.component.css']
 })
-export class FormacionTarjetaComponent implements OnInit {
+export class FormacionTarjetaComponent {
 
-  constructor() { }
+  @Input() educacion!: Educacion
+  constructor(public dialog: MatDialog) { }
 
-  ngOnInit(): void {
+  editar(id: number | undefined, titulo: string, institucion: string, fecha_inicio: Date | undefined, fecha_fin?: Date, logo?: any) {
+    const dialogRef = this.dialog.open(AgregarFormacionComponent, {
+      width: '600px',
+      data: {id: id, titulo: titulo, institucion: institucion, fecha_inicio: fecha_inicio, fecha_fin: fecha_fin, logo: logo}
+    })
+    dialogRef.afterClosed().subscribe()
   }
-
 }
