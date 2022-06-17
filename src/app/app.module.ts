1dacc97cb6f9995be7ee99a64755be7a26fd0114
Facundo Kadir Flores Ferreyra <fkfloresferreyra@outlook.com>
2022-06-17 17:46:49 -0300

    Estructura base del módulo de autenticación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MaterialModule} from "./material/material.module";
 import {HttpClientModule} from "@angular/common/http";
 import { ConfirmarComponent } from './common/confirmar/confirmar.component';
+import { AuthenticationComponent } from './common/auth/authentication/authentication.component';
+import { LoginComponent } from './common/auth/login/login.component';
+import { LogoutComponent } from './common/auth/logout/logout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConfirmarComponent
+    ConfirmarComponent,
+    AuthenticationComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     AppRoutingModule,
