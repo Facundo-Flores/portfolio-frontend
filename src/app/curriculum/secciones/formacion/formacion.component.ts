4510ed99d3e7909139ffbdc565e9b5204a5a93bc
Facundo Kadir Flores Ferreyra <fkfloresferreyra@outlook.com>
2022-06-17 09:46:08 -0300

    Creada sección Educación del curriculum: se crean dos listas, una de cursos y otra de educación formal que se llenan mediante el método GET del CurriculumService, luego se pasan cada uno a la tarjeta correspondiente

--- src/app/curriculum/secciones/formacion/formacion.component.ts
+++ src/app/curriculum/secciones/formacion/formacion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Curso, CursoResponse, Educacion, EducacionResponse} from "../../../common/interfaces/curriculum.interface";
+import {CurriculumService} from "../../../services/curriculum.service";
 
 @Component({
   selector: 'app-formacion',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormacionComponent implements OnInit {
 
-  constructor() { }
+  listaFormacion: Educacion[] = [];
+  listaCursos: Curso[] = [];
+
+  constructor(private curriculumService: CurriculumService) { }
 
   ngOnInit(): void {
+    this.getListaFormacion();
+    this.getListaCursos()
   }
 
+  getListaFormacion() {
+    this.curriculumService.getAllInfo<EducacionResponse>('educacion')
+      .subscribe(resp => {
+        this.procesarResponseEducacion(resp);
+      })
+  }
+
+  getListaCursos() {
+    this.curriculumService.getAllInfo<CursoResponse>('curso')
+      .subscribe(resp => {
+        this.procesarResponseCurso(resp)
+      })
+  }
+
+  procesarResponseEducacion(resp: EducacionResponse): Educacion[] {
+    let listaEducacion = resp.educacionList
+    listaEducacion.forEach((item: Educacion) => {
+      if (item.logo != null)
+        item.logo = 'data:image/jpeg;base64,' + item.logo
+      this.listaFormacion.push(item)
+    })
+    return this.listaFormacion
+  }
+
+  procesarResponseCurso(resp: CursoResponse): Curso[] {
+    let listaCursos = resp.cursos
+    listaCursos.forEach((item: Curso) => {
+      if (item.logo != null)
+        item.logo = 'data:image/jpeg;base64,' + item.logo
+      this.listaCursos.push(item)
+    })
+    return this.listaCursos
+  }
 }
