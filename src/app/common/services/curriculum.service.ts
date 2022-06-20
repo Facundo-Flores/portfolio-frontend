01d601995a9d572748b9b0627b34c21df62abe89
F-K-F-F <fkfloresferreyra@gmail.com>
2022-06-20 09:23:38 -0300

    correción en la ruta a la API declarada en curriculumService

--- src/app/common/services/curriculum.service.ts
+++ src/app/common/services/curriculum.service.ts
@@ -7,7 +7,7 @@ import {Observable} from "rxjs";
 })
 export class CurriculumService {
 
-  private rutaBase: string = 'http://localhost:8080'
+  private rutaBase: string = 'http://localhost:8080/api/v1'
 
   constructor(private http: HttpClient) { }
 
